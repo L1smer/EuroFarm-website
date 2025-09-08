5c0b565e1bf74928f99a368390c7befa747a4394
Maksym Krasnov <maksimkrasnov747@gmail.com>
2025-09-08 14:39:42 +0200

    fix: update layout metadata and ensure consistent background color in Pet Stars layout

--- src/app/petstars/layout.tsx
+++ src/app/petstars/layout.tsx
@@ -1,6 +1,16 @@
 import "@/app/petstars.css";
 import { montserrat } from "./lib/fonts";
 import Header from "./ui/Header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Pet Stars",
+  description: "Pet Stars website",
+  other: {
+    "apple-mobile-web-app-capable": "yes",
+    "apple-mobile-web-app-status-bar-style": "black-translucent",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -9,8 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body style={{ backgroundColor: "var(--page-bg)" }} className={`${montserrat.className} antialiased`}>
-        <Header/>
+      <body
+        style={{ backgroundColor: "var(--page-bg)" }}
+        className={`${montserrat.className} antialiased`}
+      >
+        <Header />
         {children}
       </body>
     </html>
